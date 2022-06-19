99f7391eb5eafcba652e39d92a8e8e2d23008c04
LebedevMar9n <lebedevmar9n1991@gmail.com>
2022-06-19 18:47:51 +0300

    route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Box width='400px' sx={{width:{xl:'1488px'}}} m='auto'>
       <Navbar />
       <Routes>
-        <Route path={'/gym_app'} element={<Home />} />
+        <Route path={'/gym_app/'} element={<Home />} />
         <Route path='/exercise/:id' element={<ExerciseDetail />} />
       </Routes>
       <Footer />
